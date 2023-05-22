56295bb033708f3d46d9e97ee9f0e4490d88b343
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-05-21 21:37:53 -0500

    error fix

--- Package.swift
+++ Package.swift
@@ -5,8 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NodeProtocol",
-	platforms: [
-		],
+
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library( name: "NodeProtocol", targets: ["NodeProtocol"])
